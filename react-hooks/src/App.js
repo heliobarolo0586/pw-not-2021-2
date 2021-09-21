5cf06ab198f7480bf17cbe9ced436c57d90e1c2e
heliobarolo0586 <heliobarolo0586@gmail.com>
2021-09-21 23:33:53 +0000

    Aula 21-09 state lifting e state colocation

--- react-hooks/src/App.js
+++ react-hooks/src/App.js
@@ -4,6 +4,10 @@ import Teste1 from './exercicios/Teste1'
 import Teste2 from './exercicios/Teste2'
 import Exercicio01 from './exercicios/01';
 import Exercicio02 from './exercicios/02'
+import Exercicio03 from './exercicios/03'
+import Exercicio04 from './exercicios/04'
+
+
 
 
 import './App.css';
@@ -35,6 +39,12 @@ function App() {
           <li>
             <Link to="02">Exercicio 02</Link>
           </li>
+          <li>
+            <Link to="03">Exercicio 03</Link>
+          </li>
+          <li>
+            <Link to="04">Exercicio 04</Link>
+          </li>
         </ul>
 
         <div style={{ border: '1px solid blue', padding: '20px' }}>
@@ -50,6 +60,12 @@ function App() {
             </Route>
             <Route path="/02">
               <Exercicio02 />
+            </Route>
+            <Route path="/03">
+              <Exercicio03 />
+            </Route>
+            <Route path="/04">
+              <Exercicio04 />
             </Route>
           </Switch>
         </div>
